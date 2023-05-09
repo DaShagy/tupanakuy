d383c1c0ca97c40d3dccbdbb43f64c2a3ada02dc
Juan Jose Abuin <juanj.abuin@gmail.com>
2023-05-09 13:10:08 -0300

    Classroom chat screen drafted

---
+++ app/src/main/java/com/dshagapps/tupanakuy/common/ui/component/screen/ChatScreen.kt
@@ -0,0 +1,79 @@
+package com.dshagapps.tupanakuy.common.ui.component.screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import com.dshagapps.tupanakuy.common.ui.component.button.StateButton
+import com.dshagapps.tupanakuy.common.ui.component.form.StateTextField
+import com.dshagapps.tupanakuy.common.ui.component.screen.ChatScreenId.CONTENT_LAYOUT_ID
+import com.dshagapps.tupanakuy.common.ui.component.screen.ChatScreenId.MESSAGE_INPUT_LAYOUT_ID
+import com.dshagapps.tupanakuy.common.ui.util.ButtonState
+import com.dshagapps.tupanakuy.common.ui.util.TextFieldState
+
+@Composable
+fun ChatScreen(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    ConstraintLayout(
+        modifier = modifier.fillMaxSize(),
+        constraintSet = getChatScreenConstraintSet()
+    ) {
+        Row(
+            modifier = modifier
+                .layoutId(CONTENT_LAYOUT_ID)
+                .fillMaxWidth()
+                .padding(4.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            content()
+        }
+        Row(
+            modifier = modifier
+                .layoutId(MESSAGE_INPUT_LAYOUT_ID)
+                .fillMaxWidth()
+                .padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            StateTextField(
+                modifier = Modifier.weight(1f).padding(4.dp),
+                state = TextFieldState()
+            )
+            StateButton(
+                ButtonState("Send")
+            )
+        }
+    }
+}
+
+private fun getChatScreenConstraintSet() = ConstraintSet {
+    val contentLayout = createRefFor(CONTENT_LAYOUT_ID)
+    val bottomLayout = createRefFor(MESSAGE_INPUT_LAYOUT_ID)
+
+    createVerticalChain(contentLayout, bottomLayout)
+
+    constrain(contentLayout) {
+        start.linkTo(parent.start)
+        top.linkTo(parent.top)
+        end.linkTo(parent.end)
+        bottom.linkTo(bottomLayout.top)
+        height = Dimension.fillToConstraints
+    }
+    constrain(bottomLayout) {
+        start.linkTo(parent.start)
+        top.linkTo(contentLayout.bottom)
+        end.linkTo(parent.end)
+        bottom.linkTo(parent.bottom)
+    }
+}
+
+private object ChatScreenId {
+    const val CONTENT_LAYOUT_ID = "CONTENT_LAYOUT_ID"
+    const val MESSAGE_INPUT_LAYOUT_ID = "MESSAGE_INPUT_LAYOUT_ID"
+}
--- app/src/main/java/com/dshagapps/tupanakuy/common/ui/screen/ClassroomScreen.kt
+++ app/src/main/java/com/dshagapps/tupanakuy/common/ui/screen/ClassroomScreen.kt
@@ -1,13 +1,21 @@
 package com.dshagapps.tupanakuy.common.ui.screen
 
 import android.content.Context
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.Lifecycle
+import com.dshagapps.tupanakuy.common.ui.component.form.StateTextField
 import com.dshagapps.tupanakuy.common.ui.component.loader.Loader
 import com.dshagapps.tupanakuy.common.ui.component.screen.BaseScreen
+import com.dshagapps.tupanakuy.common.ui.component.screen.ChatScreen
 import com.dshagapps.tupanakuy.common.ui.util.OnLifecycleEvent
+import com.dshagapps.tupanakuy.common.ui.util.TextFieldState
 import com.dshagapps.tupanakuy.common.ui.viewmodel.ClassroomScreenViewModel
 import kotlinx.coroutines.flow.StateFlow
 
@@ -31,10 +39,9 @@ fun ClassroomScreen(
     when (val s = state.collectAsState().value) {
         is ClassroomScreenViewModel.State.Idle -> {
             BaseScreen(title = "Classroom: ${s.classroom}") {
-
+                ChatScreen {}
             }
         }
         ClassroomScreenViewModel.State.Loading -> Loader()
     }
-
 }
